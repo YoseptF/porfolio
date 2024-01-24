012ba68932b643ed837dd9970a1ca3e88b8b6f20
YoseptF <yosept.flores@gmail.com>
2024-01-23 18:11:35 -0600

    new calendly page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
+import './index.css';
+import 'animate.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/containers/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 
   <React.StrictMode>
-    <App />
+    <div
+      style={{
+        background: '#2c2b2b',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        placeItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <img
+        className="animate__animated animate__infinite animate__bounce"
+        src="/cards/call.png"
+        alt="logo"
+      />
+    </div>
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
