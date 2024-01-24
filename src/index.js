5ddb646fc7fbfb4abf155bceb3b6ac3af34785dd
YoseptF <yosept.flores@gmail.com>
2024-01-23 19:11:22 -0600

    GET METADATA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import 'animate.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    fetch('https://formspree.io/xvovpedq', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: `someone just saw your calendly!${JSON.stringify({
+          browser: navigator.userAgent,
+          location: window.location.href,
+          referrer: document.referrer,
+          // get ip
+          ip: data.ip,
+        })}`,
+
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
+
 ReactDOM.render(
 
   <React.StrictMode>
